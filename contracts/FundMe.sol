e982dbed4857c4f61b9fe392d442552b5075cd34
bejzik8 <bejzik8@gmail.com>
2023-10-26 00:59:04 +0200

    Updated FundMe contract.

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -4,20 +4,22 @@ pragma solidity ^0.8.8;
 
 import "./PriceConverter.sol";
 
+error NotOwner();
+
 contract FundMe {
     using PriceConverter for uint256;
 
     // uint256 public number;
 
-    uint256 public minimumUsd = 50 * 1e18; // 1 * 10 ** 18
+    uint256 public constant MINIMUM_USD = 50 * 1e18; // 1 * 10 ** 18
 
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
 
-    address public owner;
+    address public immutable i_owner;
 
     constructor() {
-        owner = msg.sender;
+        i_owner = msg.sender;
     }
 
     function fund() public payable {
@@ -25,7 +27,7 @@ contract FundMe {
 
         // number = 5;
 
-        require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough!"); // 1e18 == 1 * 10 ** 18 == 1.000.000.000.000.000.000
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "Didn't send enough!"); // 1e18 == 1 * 10 ** 18 == 1.000.000.000.000.000.000
 
         // what is reverting?
         // undo any action before, and send the remaining gas back.
@@ -71,7 +73,8 @@ contract FundMe {
     }
 
     modifier onlyOwner {
-        require(msg.sender == owner, "Sender is not owner!");
+        // require(msg.sender == i_owner, "Sender is not owner!");
+        if (msg.sender != i_owner) { revert NotOwner(); }
         _;
     }
 }

3c740b6d07f9139d01cf854657921c42daac2d9e
bejzik8 <bejzik8@gmail.com>
2023-10-25 15:38:50 +0200

    Working on getPrice method...

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -2,20 +2,44 @@
 
 pragma solidity ^0.8.8;
 
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe {
     // uint256 public number;
+
+    uint256 public minimumUsd = 50;
 
     function fund() public payable {
         // want to be able to set a minimum fund amount in USD
 
         // number = 5;
 
-        require(msg.value > 1e18, "Didn't send enough!"); // 1e18 == 1 * 10 ** 18 == 1.000.000.000.000.000.000
+        require(msg.value > minimumUsd, "Didn't send enough!"); // 1e18 == 1 * 10 ** 18 == 1.000.000.000.000.000.000
 
         // what is reverting?
         // undo any action before, and send the remaining gas back.
         // reverting => number is not set to 5.
     }
+
+    function getPrice() public view returns(uint256) {
+        // ABI
+        // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+
+        // ETH in terms of USD
+        // 3.000,00000000
+        (, int256 price,,,) = priceFeed.latestRoundData();
+
+        return uint256(price * 1e10); // 1**10 == 10.000.000.000
+    }
+
+    function getVersion() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return priceFeed.version();
+    }
+
+    function getConversionRate() public {}
 
     // function withdraw() {}
 }

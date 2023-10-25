8fd133ee6ed914c1745bbc23010d22e2cf00c1e3
bejzik8 <bejzik8@gmail.com>
2023-10-26 01:08:59 +0200

    Added receive and fallback functions to the contract.

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -77,4 +77,12 @@ contract FundMe {
         if (msg.sender != i_owner) { revert NotOwner(); }
         _;
     }
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
 }

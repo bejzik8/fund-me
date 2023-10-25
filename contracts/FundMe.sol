57237456ffe5bd0a66c04fae1aa74c3b3ec70c2e
bejzik8 <bejzik8@gmail.com>
2023-10-25 15:55:53 +0200

    Created PriceConverter library.

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -2,9 +2,11 @@
 
 pragma solidity ^0.8.8;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
 
 contract FundMe {
+    using PriceConverter for uint256;
+
     // uint256 public number;
 
     uint256 public minimumUsd = 50 * 1e18; // 1 * 10 ** 18
@@ -25,31 +27,6 @@ contract FundMe {
 
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = msg.value;
-    }
-
-    function getPrice() public view returns(uint256) {
-        // ABI
-        // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
-
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-
-        // ETH in terms of USD
-        // 3.000,00000000
-        (, int256 price,,,) = priceFeed.latestRoundData();
-
-        return uint256(price * 1e10); // 1**10 == 10.000.000.000
-    }
-
-    function getVersion() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        return priceFeed.version();
-    }
-
-    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-
-        return ethAmountInUsd;
     }
 
     // function withdraw() {}
